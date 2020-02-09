26f5d2119af612f211b93b66328b3136c9b765de
Yair Aichenbaum <explortec@outlook.com>
2020-02-09 02:13:28 -0500

    Added ask to review dialog

---
+++ UserControls/AskForReviewUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWP_Visual_Asset_Generator.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP_Visual_Asset_Generator.UserControls
+{
+    public sealed partial class AskForReviewUserControl : ContentDialog
+    {
+        public MainViewModel mainViewModel => App.mainViewModel;
+
+        public AskForReviewUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void yesButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainViewModel.ReviewThisApp();
+            Hide();
+        }
+
+        private void noButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+        }
+    }
+}
